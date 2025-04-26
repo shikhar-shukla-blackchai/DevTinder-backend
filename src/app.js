022470560a559f2c5c2baa6b567faf132537d991
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2025-04-26 17:44:24 +0530

    learning advance routing techniques

--- src/app.js
+++ src/app.js
@@ -2,34 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.json({
-    message: "This is the user data",
-    useData: {
-      firstName: "Shikhar",
-      lastName: "Shukla",
-    },
-  });
-});
-
-app.post("/user", (req, res) => {
-  res.json({ message: "User data is saved successfully" });
-});
-
-app.delete("/user", (req, res) => {
-  res.json({ message: "User is deleted form the database" });
-});
-
-app.patch("/user", (req, res) => {
-  res.json({ message: "User data is successfully updated" });
-});
-
-app.put("/user", (req, res) => {
-  res.json({ message: "This user is replaced by the new user" });
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello for server 7777 : TEST");
+// working: dragonfly, butterfly
+app.get("/user/:id/:age/:gender/:password", (req, res) => {
+  console.log(req.params);
+  res.json({ fullName: "Shikhar" });
 });
 
 app.listen(7777, () => {
