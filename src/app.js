ed02f58c663fa9e4131f56a176bedd0b3f40873d
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2025-05-01 11:59:34 +0530

    Schema Validations

--- src/app.js
+++ src/app.js
@@ -16,6 +16,55 @@ app.post("/signup", async (req, res, next) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    if (users.length === 0) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).json({ message: "something went wrong" });
+  }
+});
+
+app.delete("/delete", async (req, res) => {
+  try {
+    await User.findByIdAndDelete({ _id: req.body.userId });
+    res.send("User is deleted successfully");
+  } catch (err) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find();
+    if (user.length === 0) {
+      res.send("There is no one available in your feed");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+app.patch("/update", async (req, res) => {
+  try {
+    const _id = req.body._id;
+    const updateData = req.body;
+    const newUserData = await User.findByIdAndUpdate({ _id }, updateData, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.json({ message: "data is updated successfully" });
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database is connected successfully");
--- src/model/user.js
+++ src/model/user.js
@@ -1,25 +1,55 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      minLength: 3,
+      maxLength: 40,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZwFQlGVN6PKDwi5_rGrArjUWFH3XnGcQWsQ&s",
+    },
+    about: {
+      type: String,
+      default: "This is default About section provided by the Backend",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
