275fce72e9cf54d3c36abe793ecc1aa7686dcd29
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2025-05-06 18:44:37 +0530

    Handle incoming connection request

--- src/routes/request.js
+++ src/routes/request.js
@@ -58,4 +58,46 @@ requestRouter.post(
   }
 );
 
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+
+      const allowedStatus = ["accepted", "rejected"];
+
+      const { status, requestId } = req.params;
+
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Status not allowed " + status });
+      }
+
+      const connectionRequest = await ConnectionRequestModule.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found" });
+      }
+
+      connectionRequest.status = status;
+
+      await connectionRequest.save();
+
+      res.send({
+        message: loggedInUser.firstName + " " + status + " Your request",
+      });
+    } catch (err) {
+      res.status(400).send({ message: "ERROR :" + err.message });
+    }
+  }
+);
+
 module.exports = { requestRouter };
